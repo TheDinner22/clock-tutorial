52d2aa2f7a2a80107a69749d25e2eb1cfec689d1
joe goodman <jgood1133@gmail.com>
2022-11-10 20:30:22 -0500

    done!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use chrono::Local;
+
+// https://www.w3.org/TR/xml-entity-names/025.html
+const DIGITS : [[&str; 11]; 7] = [
+    ["┏━┓ ","  ╻  "," ┏━┓ ", " ┏━┓ "," ╻ ╻ "," ┏━┓ "," ┏   "," ┏━┓ "," ┏━┓ "," ┏━┓ ","   "],
+    ["┃ ┃ ","  ┃  ","   ┃ ", "   ┃ "," ┃ ┃ "," ┃   "," ┃   ","   ┃ "," ┃ ┃ "," ┃ ┃ "," ╻ "],
+    ["┃ ┃ ","  ┃  ","   ┃ ", "   ┃ "," ┃ ┃ "," ┃   "," ┃   ","   ┃ "," ┃ ┃ "," ┃ ┃ ","   "],
+    ["┃ ┃ ","  ┃  "," ┏━┛ ", " ┣━┫ "," ┗━┫ "," ┗━┓ "," ┣━┓ ","   ┃ "," ┣━┫ "," ┗━┫ ","   "],
+    ["┃ ┃ ","  ┃  "," ┃   ", "   ┃ ","   ┃ ","   ┃ "," ┃ ┃ ","   ┃ "," ┃ ┃ ","   ┃ ","   "],
+    ["┃ ┃ ","  ┃  "," ┃   ", "   ┃ ","   ┃ ","   ┃ "," ┃ ┃ ","   ┃ "," ┃ ┃ ","   ┃ "," ╹ "],
+    ["┗━┛ ","  ╹  "," ┗━━ ", " ┗━┛ ","   ╹ "," ┗━┛ "," ┗━┛ ","   ╹ "," ┗━┛ "," ┗━┛ ","   "],
+];
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+        // time object with date and time and all that
+        let t = Local::now();
+
+        // format this time into a string with only hours mins seconds
+        let time = t.format("%H:%M:%S").to_string();
+
+        for row in &DIGITS {
+            for c in time.chars() {
+                let col = match c {
+                    '0'..='9' => c as usize - '0' as usize,
+                    ':' => 10,
+                    _ => panic!(" got some other char I was not expecting")
+                };
+                print!("{} ", row[col]);
+            }
+            println!();
+        }
+
+        // sleep the thread for some number or miliseconds
+        let sleep = 999;
+        std::thread::sleep(std::time::Duration::from_millis(sleep));
+    }
 }
